f7101fdd0e58ea726e36cd10796fb02da7fdc441
josemariandreu <73637226+josemariandreu@users.noreply.github.com>
2021-01-11 22:01:53 +0100

    20 contenido

---
+++ ejercicios/EJ_20_cookies/js/cookies.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+function controlcookies() {
+         // si variable no existe se crea (al clicar en Aceptar)
+    localStorage.controlcookie = (localStorage.controlcookie || 0);
+ 
+    localStorage.controlcookie++; // incrementamos cuenta de la cookie
+    cookie1.style.display='none'; // Esconde la política de cookies
+}
+if (localStorage.controlcookie>0){ 
+document.getElementById('cookie1').style.bottom = '-50px';
+}
